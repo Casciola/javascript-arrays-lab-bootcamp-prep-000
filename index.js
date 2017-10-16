1a5c13156f615fd95b6768d8d4c03f1e88044343
Nick Casciola <s0948644@monmouth.edu>
2017-10-16 00:42:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+};
+
+function destructivelyPrependKitten(name) {
+
+};
+
+function destructivelyRemoveLastKitten() {
+
+};
+
+function appendKitten(name) {
+
+};
+
+function prependKitten(name) {
+
+};
+
+function removeLastKitten() {
+
+};
+
+function removeFirstKitten() {
+
+};
