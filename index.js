1701b466342caf966bb9f6fc3478906340407ac5
Nick Casciola <s0948644@monmouth.edu>
2017-10-16 00:53:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,9 +33,11 @@ function prependKitten(name) {
 };
 
 function removeLastKitten() {
-
+  var newKittens = [];
+  return newKittens = kittens.slice(0, kittens.length - 1);
 };
 
 function removeFirstKitten() {
-
+  var newKittens = [];
+  return newKittens = kittens.slice(1);
 };
